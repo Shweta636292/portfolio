ab3cc512487a8532646741f3ddec4c75cbc8c679
Shweta636292 <shwetagejjihalli@gmail.com>
2026-01-14 23:40:58 +0530

    scopes.js

---
+++ Shweta/11_scopes.js
@@ -0,0 +1,37 @@
+let a=300
+if(true){
+    let a=9
+    const b=89
+    console.log("INNER:",a)
+}
+console.log(a)
+
+function one(){
+    const UserName="shweta"
+
+    function Two(){
+        const website="youtube"
+        console.log(UserName)
+    }
+    Two()
+}
+one()
+
+let userName;
+let website;
+
+if(true){
+    const UserName="shweta";
+    if(UserName==="shweta"){
+        const website="youtube";
+        console.log(UserName+ website)
+    }
+    console.log(website)
+}
+console.log(userName)
+
+console.log(addone(5))
+
+function addone(num){
+    return num+1
+}
